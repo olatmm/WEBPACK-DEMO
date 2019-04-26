472a305e6a9f41fabb56e48fa9fd77da0cf9ce1d
Sanni <ajibolas@amazon.com>
2019-04-26 02:48:06 -0400

    third commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import _ from 'lodash';
 import './style.css';
-import 'bootstrap';
-import Icon from './'
+import Icon from './webpack.png';
+import Data from './data.xml';
+import Report from  './report.json';
 
 function component(){
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack', '']);
     element.classList.add(['hello', 'jumbotron']);
-  
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+    console.log(Report);
     return element;
 }
 
